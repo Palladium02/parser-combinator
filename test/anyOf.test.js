78e4b27c3830c315e9926cb6002fabb7c1b20c40
Palladium02 <seanmeyer544@gmail.com>
2023-07-17 15:50:20 +0200

    added tests for anyOf parser

---
+++ test/anyOf.test.js
@@ -0,0 +1,30 @@
+const {describe, it} = require('node:test');
+const assert = require('assert');
+const {anyOf, char} = require('../build');
+
+describe('Testing char parser', () => {
+  const parser = anyOf(char('a'), char('b'));
+  it('Should parse single character', () => {
+    const {result} = parser.parse('a');
+
+    assert.equal(result, 'a');
+  });
+
+  it('Should also parse the other option', () => {
+    const {result} = parser.parse('b');
+
+    assert.equal(result, 'b');
+  });
+
+  it('Should parse and return rest', () => {
+    const {rest} = parser.parse('ab');
+
+    assert.equal(rest, 'b');
+  });
+
+  it('Should not parse wrong character', () => {
+    const {error} = parser.parse('c');
+
+    assert.notEqual(error, null);
+  });
+});
