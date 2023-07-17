8d26f2afb43e218403c90e0429e2c6f1c64e5bb2
Palladium02 <seanmeyer544@gmail.com>
2023-07-17 16:03:31 +0200

    added tests for nOf parser

---
+++ test/nOf.test.js
@@ -0,0 +1,24 @@
+const {describe, it} = require('node:test');
+const assert = require('assert');
+const {nOf, char} = require('../build');
+
+describe('Testing char parser', () => {
+  const parser = nOf(2, char('a'));
+  it('Should parse correct input', () => {
+    const {result} = parser.parse('aa');
+
+    assert.equal(result, 'aa');
+  });
+
+  it('Should ignore third matching character', () => {
+    const {rest} = parser.parse('aaa');
+
+    assert.equal(rest, 'a');
+  });
+
+  it('Should not parse wrong input', () => {
+    const {error} = parser.parse('a');
+
+    assert.notEqual(error, null);
+  });
+});
